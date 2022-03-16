84f076352e5a001fad4f1f847ded8a221046b24a
otsuki <takashi_otsuki@outlook.com>
2022-03-16 07:17:55 +0000

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+ 
+setuptools.setup(
+    name="aiwolf",
+    version="1.0-beta",
+    author="OTSUKI Takashi",
+    author_email="aiwolfsharp@outlook.com",
+    description="The package for creating AIWolf agent in Python",
+    long_description="This is a Python port of AIWolfLib, a .NET package.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/AIWolfSharp/aiwolf-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: Apache-2.0",
+        "Operating System :: OS Independent",
+    ]
+)
