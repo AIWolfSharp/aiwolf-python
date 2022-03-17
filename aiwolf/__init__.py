9f78074be06fdab7966034e091bef1b3c0f0f30e
otsuki <takashi_otsuki@outlook.com>
2022-03-17 19:59:44 +0900

    Modify __init__.py for seting up namespace.

--- aiwolf/__init__.py
+++ aiwolf/__init__.py
@@ -1,5 +1,5 @@
 #
-# init.py
+# __init__.py
 #
 # Copyright 2022 OTSUKI Takashi
 #
@@ -14,3 +14,46 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from aiwolf.agent import Agent as Agent
+from aiwolf.agent import Role as Role
+from aiwolf.agent import Species as Species
+from aiwolf.agent import Status as Status
+from aiwolf.client import TcpipClient as TcpipClient
+from aiwolf.constant import Constant as Constant
+from aiwolf.content import AgreeContentBuilder as AgreeContentBuilder
+from aiwolf.content import AndContentBuilder as AndContentBuilder
+from aiwolf.content import AttackContentBuilder as AttackContentBuilder
+from aiwolf.content import AttackedContentBuilder as AttackedContentBuilder
+from aiwolf.content import BecauseContentBuilder as BecauseContentBuilder
+from aiwolf.content import ComingoutContentBuilder as ComingoutContentBuilder
+from aiwolf.content import Content as Content
+from aiwolf.content import ContentBuilder as ContentBuilder
+from aiwolf.content import DayContentBuilder as DayContentBuilder
+from aiwolf.content import DisagreeContentBuilder as DisagreeContentBuilder
+from aiwolf.content import DivinationContentBuilder as DivinationContentBuilder
+from aiwolf.content import DivinedResultContentBuilder as DivinedResultContentBuilder
+from aiwolf.content import EmptyContentBuilder as EmptyContentBuilder
+from aiwolf.content import EstimateContentBuilder as EstimateContentBuilder
+from aiwolf.content import GuardContentBuilder as GuardContentBuilder
+from aiwolf.content import GuardedAgentContentBuilder as GuardedAgentContentBuilder
+from aiwolf.content import IdentContentBuilder as IdentContentBuilder
+from aiwolf.content import InquiryContentBuilder as InquiryContentBuilder
+from aiwolf.content import NotContentBuilder as NotContentBuilder
+from aiwolf.content import Operator as Operator
+from aiwolf.content import OrContentBuilder as OrContentBuilder
+from aiwolf.content import OverContentBuilder as OverContentBuilder
+from aiwolf.content import RequestContentBuilder as RequestContentBuilder
+from aiwolf.content import SkipContentBuilder as SkipContentBuilder
+from aiwolf.content import Topic as Topic
+from aiwolf.content import VoteContentBuilder as VoteContentBuilder
+from aiwolf.content import VotedContentBuilder as VotedContentBuilder
+from aiwolf.content import XorContentBuilder as XorContentBuilder
+from aiwolf.gameinfo import GameInfo as GameInfo
+from aiwolf.gamesetting import GameSetting as GameSetting
+from aiwolf.judge import Judge as Judge
+from aiwolf.player import AbstractPlayer as AbstractPlayer
+from aiwolf.utterance import Talk as Talk
+from aiwolf.utterance import Utterance as Utterance
+from aiwolf.utterance import UtteranceType as UtteranceType
+from aiwolf.utterance import Whisper as Whisper
+from aiwolf.vote import Vote as Vote
