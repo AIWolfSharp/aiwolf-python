7eef751bfaa0e3016d5c8bb09225529ec9f0381f
otsuki <takashi_otsuki@outlook.com>
2022-03-18 15:20:39 +0900

    Updated setup.py for releasing 1.0.0.

--- setup.py
+++ setup.py
@@ -2,17 +2,21 @@ import setuptools
  
 setuptools.setup(
     name="aiwolf",
-    version="0.9.1",
+    version="1.0.0",
     author="OTSUKI Takashi",
     author_email="aiwolfsharp@outlook.com",
-    description="The package for creating AIWolf agent in Python",
-    long_description="This is a Python port of AIWolfLib, a .NET package.",
-    long_description_content_type="text/markdown",
     url="https://github.com/AIWolfSharp/aiwolf-python",
+    description="The package for creating AIWolf agent in Python",
+#    long_description="This is a Python port of AIWolfLib, a .NET package.",
+    long_description_content_type="text/markdown",
+    license="Apache Software License",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: Apache-2.0",
+    classifiers=[ # see https://pypi.org/pypi?:action=list_classifiers
+        # "Development Status :: 3 - Alpha",
+        # "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
-    ]
+        "Programming Language :: Python :: 3.8"
+    ],
 )
