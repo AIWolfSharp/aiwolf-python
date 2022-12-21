35f85321a19c76dcdcb3f4eb85c24a610d3187b4
otsuki <takashi_otsuki@outlook.com>
2022-12-21 17:26:11 +0900

    Fix a error in setup.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="aiwolf",
     version="1.2.0",
-    python_requires="=3.7.*",
+    python_requires=">=3.7",
     author="OTSUKI Takashi",
     author_email="aiwolfsharp@outlook.com",
     url="https://github.com/AIWolfSharp/aiwolf-python",
