494eadc13982677bd1214399dbdc3b97a07b0354
otsuki <takashi_otsuki@outlook.com>
2024-10-14 01:47:22 +0900

    Raise version to 1.2.2.

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 import setuptools
- 
+
 setuptools.setup(
     name="aiwolf",
-    version="1.2.1",
+    version="1.2.2",
     author="OTSUKI Takashi",
     author_email="aiwolfsharp@outlook.com",
     url="https://github.com/AIWolfSharp/aiwolf-python",
     description="The package for creating AIWolf agent in Python",
-#    long_description="This is a Python port of AIWolfLib, a .NET package.",
+    #    long_description="This is a Python port of AIWolfLib, a .NET package.",
     long_description_content_type="text/markdown",
     license="Apache Software License",
     packages=setuptools.find_packages(),
     package_data={
         "aiwolf": ["py.typed"],
     },
-    classifiers=[ # see https://pypi.org/pypi?:action=list_classifiers
+    classifiers=[  # see https://pypi.org/pypi?:action=list_classifiers
         # "Development Status :: 3 - Alpha",
         # "Development Status :: 4 - Beta",
         "Development Status :: 5 - Production/Stable",
